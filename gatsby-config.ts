a0848ba7835e1291d9506595c2013a2cc50787c1
Jacky Tsang <szechunt@gmail.com>
2022-10-21 22:18:39 +0800

    udpate config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Tsang Sze Chun`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://tsangszechun.com`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -12,7 +12,26 @@ const config: GatsbyConfig = {
   plugins: ["gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      name: "Tsang Sze Chun",
+      short_name: "Jacky Tsang",
+      description: "Personal website of Tsang Sze Chun",
+      start_url: "/",
+      background_color: "#a0d8f1",
+      theme_color: "#e07628",
+      display: 'standalone',
+      icons: [
+        {
+          src: '/favicons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/favicons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+
     }
   }]
 };
