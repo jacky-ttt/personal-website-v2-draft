2d5ca1183d2515441f7e8493a297795158e99db3
Jacky Tsang <szechunt@gmail.com>
2022-10-21 22:30:58 +0800

    add tailwind

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,31 +9,35 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      name: "Tsang Sze Chun",
-      short_name: "Jacky Tsang",
-      description: "Personal website of Tsang Sze Chun",
-      start_url: "/",
-      background_color: "#a0d8f1",
-      theme_color: "#e07628",
-      display: 'standalone',
-      icons: [
-        {
-          src: '/favicons/android-chrome-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: '/favicons/android-chrome-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-      ],
+  plugins: [
+    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: "Tsang Sze Chun",
+        short_name: "Jacky Tsang",
+        description: "Personal website of Tsang Sze Chun",
+        start_url: "/",
+        background_color: "#a0d8f1",
+        theme_color: "#e07628",
+        display: 'standalone',
+        icons: [
+          {
+            src: '/favicons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/favicons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
 
-    }
-  }]
+      }
+    },
+    'gatsby-plugin-postcss',
+  ]
 };
 
 export default config;
